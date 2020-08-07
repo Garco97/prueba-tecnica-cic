b14a5484c541aae037982d6a9bee9d16874df849
Diego Garcia <dgarciacosio@gmail.com>
2020-08-07 14:22:14 +0200

    Añadida pregunta 4

---
+++ pregunta4/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function checkInput(){
+    let codigo = document.getElementById("codigo");
+    console.log(codigo.value)
+    let regex = /^[a-zA-Z]{3}\d{3}$/;
+    let match = regex.exec(codigo.value)
+    console.log(match)
+      codigo.style.backgroundColor = match ? "green": "red";
+      if(!match){
+        alert("ERROR Patrón incorrecto")
+      }
+  }
+  return (
+    <div className="App">
+      <fieldset>
+      <label htmlFor="contactName">Código</label>
+      <input type="text" defaultValue="" size="35" id="codigo" name="codigo"/>
+      <button id="sendButton" onClick={checkInput}>Enviar</button>
+      </fieldset>
+    </div>
+  );
+}
+
+export default App;
